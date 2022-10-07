86aa6648077517027aa8010a00b4954b7e001846
masha14013 <masha14013@rambler.ru>
2022-10-07 12:27:17 +0300

    added file htmlspecialchars.js

---
+++ htmlspecialchars.js
@@ -0,0 +1,7 @@
+function htmlspecialchars(formData) {
+  formData = formData.replace(/&/gi, '&amp;');
+  formData = formData.replace(/</gi, '&lt;');
+  formData = formData.replace(/>/gi, '&gt;');
+  formData = formData.replace(/"/gi, '&quot;');
+  return formData;
+}
